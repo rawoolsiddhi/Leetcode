5d5d77621506d88793c559f7e75b009a7d6b41ec
siddhi rawool <rawoolsiddhi814@gmail.com>
2023-08-19 13:49:10 +0530

    Update kth_largest.cpp

--- kth_largest.cpp
+++ kth_largest.cpp
@@ -1,1 +1,37 @@
+/*Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+      
+       /* method 1*/
 
+        // int n = size(nums);
+        // sort(nums.begin(), nums.end());
+        // return nums[n-k];
+        
+
+        /* method 2*/
+
+        // priority_queue<int> pq(nums.begin(), nums.end());
+
+        // int ans =0;
+        // while(k--){
+        //     ans=pq.top();
+        //     pq.pop();
+        // }
+        // return ans;
+
+
+        /*method 3*/
+        priority_queue<int, vector<int>, greater<int>> pq;    // min_heap
+        for(auto x: nums){           
+            pq.push(x);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        return pq.top();
+    }
+};
