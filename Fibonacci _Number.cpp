be6f9235fbbef031a7c52c9c5f1d0077466f3c2a
siddhi rawool <rawoolsiddhi814@gmail.com>
2023-09-11 18:58:45 +0530

    Update Fibonacci _Number.cpp

--- Fibonacci _Number.cpp
+++ Fibonacci _Number.cpp
@@ -1,25 +1,9 @@
-/*Given an integer x, return true if x is a 
-palindrome
-, and false otherwise.
+//The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, 
+// starting from 0 and 1. That is,
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
 
-Example 1:
-
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
-Example 2:
-
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
-
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-
- Constraints:
--231 <= x <= 231 - 1*/
 
 class Solution {
 public:
