2d0501e5c467364d351083c980e231a28ee226b4
siddhi rawool <rawoolsiddhi814@gmail.com>
2023-09-21 08:10:25 +0530

    Create Palindrome_Number.cpp

---
+++ Palindrome_Number.cpp
@@ -0,0 +1,25 @@
+// Input: x = 121
+// Output: true
+// Input: x = 10
+// Output: false
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+};
