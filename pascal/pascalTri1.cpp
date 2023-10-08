4024b1a37fb9be586ed02b1488a4ac96b3b7728c
siddhi rawool <rawoolsiddhi814@gmail.com>
2023-10-08 11:23:17 +0530

    Create pascalTri1.cpp

---
+++ pascal/pascalTri1.cpp
@@ -0,0 +1,34 @@
+//print element at particular place in pascal triangle.
+//brute ---nCr = n! / (r! * (n-r)!)
+
+
+#include<iostream>
+using namespace std;
+
+int ncr(int n, int r){
+  long long res =1;
+   for(int i=0; i< r; i++){
+     res =res *(n-i);
+     res=res/(i+1);
+   }
+
+  return res;
+  
+}
+
+int pascalTri(int r, int c){
+  int ele = ncr(r-1, c-1);
+  return ele;
+}
+int main(){
+  int r,c;
+  cout<<"enter r";
+  cin>>r;
+  cout<<"enter c";
+  cin>>c;
+
+  int element = pascalTri(r, c);
+  cout<<"The element at position (" << r << "," << c << ") is: " << element << "\n";
+  
+  return 0;
+}
